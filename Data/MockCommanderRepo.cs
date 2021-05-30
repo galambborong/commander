b5f3331af9a85aa8370287b9ac5c4260a065523c
Peter Keenan <keenan@posteo.net>
2021-05-30 11:47:39 +0100

    Tutorial up to part 2

---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using Commander.Models;
+using Microsoft.VisualBasic.FileIO;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAllCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command {Id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle and Pan"},
+                new Command {Id = 1, HowTo = "Cut bread", Line = "Get a knife", Platform = "knife and chopping board"},
+                new Command {Id = 2, HowTo = "Make a cup of tea", Line = "Pour boiling water over bag", Platform = "Tea bag and kettle"},
+            };
+
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command{Id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle and Pan"};
+        }
+    }
+}
