2281e59210c6d269e040adc0b8144b54162c4380
Peter Keenan <keenan@posteo.net>
2021-05-31 15:22:16 +0100

    Add Users model

---
+++ Models/Users.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Commander.Models
+{
+    public class Users
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [MaxLength(15)]
+        public string FirstName { get; set; }
+
+        [MaxLength(15)]
+        public string LastName { get; set; }
+        
+        [Required]
+        [MaxLength(25)]
+        public string Username { get; set; }
+        
+        [Timestamp]
+        public byte[] CreatedAt { get; set; }
+    }
+}
