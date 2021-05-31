194f1f961246548130ce2fc0e29294bc9a6a1dd7
Peter Keenan <keenan@posteo.net>
2021-05-31 10:24:10 +0100

    Add CommandReadDto mapper to ActionResult for /{id} endpoint

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -1,39 +1,43 @@
 using System.Collections.Generic;
+using AutoMapper;
 using Commander.Data;
+using Commander.Dtos;
 using Commander.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Commander.Controllers
 {
-  [Route("/api/commands")]
-  [ApiController]
-  public class CommandsController : ControllerBase
-  {
-    private readonly ICommanderRepo _repository;
-
-    public CommandsController(ICommanderRepo repository)
+    [Route("/api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
     {
-      _repository = repository;
-    }
-    
-    [HttpGet]
-    public ActionResult<IEnumerable<Command>> GetAllCommands()
-    {
-      var commandItems = _repository.GetAllCommands();
-      return Ok(commandItems);
-    }
+        private readonly ICommanderRepo _repository;
+        private readonly IMapper _mapper;
 
-    [HttpGet("{id}")]
-    public ActionResult<Command> GetCommandById(int id)
-    {
-      var commandItem = _repository.GetCommandById(id);
-      if (commandItem != null)
-      {
-        return Ok(commandItem);
-        
-      }
+        public CommandsController(ICommanderRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
 
-      return NotFound();
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAllCommands();
+            return Ok(commandItems);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<CommandReadDto> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+            if (commandItem != null)
+            {
+                return Ok(_mapper.Map<CommandReadDto>(commandItem));
+
+            }
+
+            return NotFound();
+        }
     }
-  }
 }
