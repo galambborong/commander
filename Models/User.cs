6139fe23aed4efa190acc2bb728b40d9a3196a54
Peter Keenan <keenan@posteo.net>
2021-05-31 15:23:30 +0100

    Change Users to User in Models

--- Models/User.cs
+++ Models/User.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Commander.Models
 {
-    public class Users
+    public class User
     {
         [Key]
         public int Id { get; set; }
