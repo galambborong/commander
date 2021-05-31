aa8ad1cb387573d0cc51ed10d2bc3e040c19cf68
Peter Keenan <keenan@posteo.net>
2021-05-31 12:41:27 +0100

    Add CreatedAtRoute return for POST method, giving 201 response and generating URL in headers

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -27,7 +27,7 @@ namespace Commander.Controllers
             return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commandItems));
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name="GetCommandById")]
         public ActionResult<CommandReadDto> GetCommandById(int id)
         {
             var commandItem = _repository.GetCommandById(id);
@@ -45,7 +45,9 @@ namespace Commander.Controllers
             _repository.CreateCommand(commandModel);
             _repository.SaveChanges();
 
-            return Ok(commandModel);
+            var commandReadDto = _mapper.Map<CommandReadDto>(commandModel);
+            
+            return CreatedAtRoute(nameof(GetCommandById), new {Id = commandReadDto.Id}, commandReadDto);
         }
     }
 }
