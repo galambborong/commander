0971a0ad1d1202ce1c7c1d2ef5c1084e5ff24084
Peter Keenan <keenan@posteo.net>
2021-05-31 10:19:18 +0100

    Add NotFound logic to CommandsController

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -15,8 +15,7 @@ namespace Commander.Controllers
     {
       _repository = repository;
     }
-
-    // private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+    
     [HttpGet]
     public ActionResult<IEnumerable<Command>> GetAllCommands()
     {
@@ -28,7 +27,13 @@ namespace Commander.Controllers
     public ActionResult<Command> GetCommandById(int id)
     {
       var commandItem = _repository.GetCommandById(id);
-      return Ok(commandItem);
+      if (commandItem != null)
+      {
+        return Ok(commandItem);
+        
+      }
+
+      return NotFound();
     }
   }
 }
