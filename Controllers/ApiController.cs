92353347a9845c5d025c9e110f61c28bb22fd875
Peter Keenan <keenan@posteo.net>
2021-06-10 15:04:18 +0100

    Add first draft at endpoints.json response for /api GET

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -1,4 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing.Matching;
+using static System.IO.File;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Commander.Controllers
 {
@@ -6,11 +10,12 @@ namespace Commander.Controllers
     [ApiController]
     public class ApiController : Controller
     {
-        // GET
         public IActionResult Index()
         {
-            string response = "hello";
-            return Ok(response);
+            const string fileName = "endpoints.json";
+ 
+            // TODO Look into returning the file properly, so it as actual JSON
+            return Ok(ReadAllText(fileName));
         }
     }
 }
