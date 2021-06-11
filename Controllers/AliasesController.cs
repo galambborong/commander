9b5ad6fd15e1610730dcd011d86125ed1db5e312
Peter Keenan <keenan@posteo.net>
2021-06-11 11:36:43 +0100

    Add first alias endpoint with MockAliasesRepo

---
+++ Controllers/AliasesController.cs
@@ -0,0 +1,28 @@
+using AutoMapper;
+using Commander.Data;
+using Commander.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Commander.Controllers
+{
+    [Route("/api/commands/{id}/alias")]
+    [ApiController]
+    public class AliasesController : ControllerBase
+    {
+        private readonly IAliasesRepo _aliasesRepo;
+        private readonly IMapper _mapper;
+
+        public AliasesController(IAliasesRepo aliasesRepo, IMapper mapper)
+        {
+            _mapper = mapper;
+            _aliasesRepo = aliasesRepo;
+        }
+        
+        [HttpGet]
+        public ActionResult<AliasReadDto> GetAliasByCommandId(int id)
+        {
+            var aliasItems = _aliasesRepo.GetAliasByCommandId(id);
+            return Ok(aliasItems);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -39,6 +39,7 @@ namespace Commander
 
             services.AddScoped<ICommandsRepo, PsqlCommandsRepo>();
             services.AddScoped<IPlatformsRepo, PsqlPlatformsRepo>();
+            services.AddScoped<IAliasesRepo, MockAliasesRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
