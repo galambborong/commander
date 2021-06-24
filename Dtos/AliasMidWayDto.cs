0e9ca34525292e3ea83b1ee26f65dd21d6b1db23
Peter Keenan <keenan@posteo.net>
2021-06-24 17:34:38 +0100

    Add AliasMidWayDto

---
+++ Dtos/AliasMidWayDto.cs
@@ -0,0 +1,11 @@
+namespace Commander.Dtos
+{
+    public class AliasMidWayDto
+    {
+        public int Id { get; set; }
+        public string CommandAlias { get; set; }
+        public string Command { get; set; }
+        public int CommandId { get; set; }
+    }
+    
+}
