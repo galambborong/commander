417f119913a6869158935ceef317d6d4e5feb6b0
Peter Keenan <keenan@posteo.net>
2021-09-14 08:51:00 +0100

    Early notes on AAA method and naming conventions for testing web API

---
+++ test/Commander.Tests/PlatformsControllerTests.cs
@@ -0,0 +1,27 @@
+using System;
+using Xunit;
+
+namespace Commander.Tests
+{
+    public class PlatformsControllerTests
+    {
+        [Fact]
+        // Naming convention for tests:
+        // UnitOfWork
+        // StateUnderTest
+        // ExpectedBehaviour
+        public void GetPlatformByIdAsync_NonexistantItem_ReturnsNotFound()
+        {
+            // Arrange
+            
+            
+            
+            // Act
+            
+            // Assert
+        }
+    }
+}
+
+
+// https://www.youtube.com/watch?v=dsD0CMgPjUk at 24'32"
