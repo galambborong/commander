2a86e2661fb8ceecca40b8aace1817cca9081c0d
Peter Keenan <keenan@posteo.net>
2022-03-26 16:55:06 +0000

    Package updates and seed data with migration

--- src/Commander/Data/CommanderContext.cs
+++ src/Commander/Data/CommanderContext.cs
@@ -5,12 +5,51 @@ namespace Commander.Data
 {
     public class CommanderContext : DbContext
     {
+        public DbSet<Command> Commands { get; set; }
+        public DbSet<Platform> Platforms { get; set; }
+        public DbSet<Alias> Aliases { get; set; }
         public CommanderContext(DbContextOptions<CommanderContext> opt) : base(opt)
         {
         }
 
-        public DbSet<Command> Commands { get; set; }
-        public DbSet<Platform> Platforms { get; set; }
-        public DbSet<Alias> Aliases { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Platform>().HasData(
+                new Platform
+                {
+                    Id = 1,
+                    Name = "Linux"
+                },
+                new Platform
+                {
+                    Id = 2,
+                    Name = "Git"
+                },
+                new Platform
+                {
+                    Id = 3,
+                    Name = "DotNet"
+                }
+            );
+
+            modelBuilder.Entity<Command>().HasData(
+                new Command
+                {
+                    Id = 1,
+                    Line = "git status",
+                    HowTo = "Check git repository status",
+                    PlatformId = 2,
+                },
+                new Command
+                {
+                    Id = 2,
+                    Line = "df -h",
+                    HowTo = "Check disk usage",
+                    PlatformId = 1,
+                }
+            );
+            
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ src/Commander/Migrations/20220326164951_TrySeedData.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Commander.Migrations
+{
+    public partial class TrySeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Platforms",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Linux" },
+                    { 2, "Git" },
+                    { 3, "DotNet" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Commands",
+                columns: new[] { "Id", "AdminPrivilegesRequired", "HowTo", "Line", "PlatformId" },
+                values: new object[,]
+                {
+                    { 1, false, "Check git repository status", "git status", 2 },
+                    { 2, false, "Check disk usage", "df -h", 1 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Commands",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Commands",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Platforms",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Platforms",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Platforms",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
