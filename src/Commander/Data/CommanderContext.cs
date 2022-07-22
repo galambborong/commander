14aec84bc113a383b9b80148aaecaf7eb6d4d6ef
Peter Keenan <keenan@posteo.net>
2022-07-22 22:54:33 +0100

    Play with bridging containers

--- src/Commander/Data/CommanderContext.cs
+++ src/Commander/Data/CommanderContext.cs
@@ -13,80 +13,80 @@ public class CommanderContext : DbContext
     {
     }
 
-    // protected override void OnModelCreating(ModelBuilder modelBuilder)
-    // {
-    //     modelBuilder.Entity<Platform>().HasData(
-    //         new Platform
-    //         {
-    //             Id = 1,
-    //             Name = "Linux HELLO FROM HERE"
-    //         },
-    //         new Platform
-    //         {
-    //             Id = 2,
-    //             Name = "Git"
-    //         },
-    //         new Platform
-    //         {
-    //             Id = 3,
-    //             Name = "DotNet"
-    //         },
-    //         new Platform
-    //         {
-    //             Id = 4,
-    //             Name = "Shell"
-    //         },
-    //         new Platform
-    //         {
-    //             Id = 5,
-    //             Name = "Arduino"
-    //         },
-    //         new Platform
-    //         {
-    //             Id = 6,
-    //             Name = "Docker"
-    //         }
-    //     );
-    //
-    //     modelBuilder.Entity<Command>().HasData(
-    //         new Command
-    //         {
-    //             Id = 1,
-    //             Line = "git status",
-    //             HowTo = "Check git repository status",
-    //             PlatformId = 2,
-    //         },
-    //         new Command
-    //         {
-    //             Id = 2,
-    //             Line = "df -h",
-    //             HowTo = "Check disk usage",
-    //             PlatformId = 1,
-    //         },
-    //         new Command
-    //         {
-    //             Id = 3,
-    //             Line =
-    //                 "docker run -d --rm -p <ext>:<int> -e Logging__Console__FormatterName=\"\" <namespace>/<image>:<tag>",
-    //             HowTo = "Run Docker Image in background; removing container once finished; with nice logging format",
-    //             PlatformId = 6
-    //         },
-    //         new Command
-    //         {
-    //             Id = 4,
-    //             Line = "docker ps",
-    //             HowTo = "List current Docker processes (containers)",
-    //             PlatformId = 6
-    //         },
-    //         new Command
-    //         {
-    //             Id = 5,
-    //             Line = "docker build <pwd> -t <namespace>/<image>:<tag>",
-    //             HowTo = "Build Docker image from Dockerfile",
-    //             PlatformId = 6
-    //         }
-    //     );
-    //
-    //     base.OnModelCreating(modelBuilder);
-    // }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Platform>().HasData(
+            new Platform
+            {
+                Id = 1,
+                Name = "Linux HELLO FROM HERE"
+            },
+            new Platform
+            {
+                Id = 2,
+                Name = "Git"
+            },
+            new Platform
+            {
+                Id = 3,
+                Name = "DotNet"
+            },
+            new Platform
+            {
+                Id = 4,
+                Name = "Shell"
+            },
+            new Platform
+            {
+                Id = 5,
+                Name = "Arduino"
+            },
+            new Platform
+            {
+                Id = 6,
+                Name = "Docker"
+            }
+        );
+
+        modelBuilder.Entity<Command>().HasData(
+            new Command
+            {
+                Id = 1,
+                Line = "git status",
+                HowTo = "Check git repository status",
+                PlatformId = 2,
+            },
+            new Command
+            {
+                Id = 2,
+                Line = "df -h",
+                HowTo = "Check disk usage",
+                PlatformId = 1,
+            },
+            new Command
+            {
+                Id = 3,
+                Line =
+                    "docker run -d --rm -p <ext>:<int> -e Logging__Console__FormatterName=\"\" <namespace>/<image>:<tag>",
+                HowTo = "Run Docker Image in background; removing container once finished; with nice logging format",
+                PlatformId = 6
+            },
+            new Command
+            {
+                Id = 4,
+                Line = "docker ps",
+                HowTo = "List current Docker processes (containers)",
+                PlatformId = 6
+            },
+            new Command
+            {
+                Id = 5,
+                Line = "docker build <pwd> -t <namespace>/<image>:<tag>",
+                HowTo = "Build Docker image from Dockerfile",
+                PlatformId = 6
+            }
+        );
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
--- src/Commander/Startup.cs
+++ src/Commander/Startup.cs
@@ -5,63 +5,57 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Commander.Data;
-using Commander.Models;
 using Commander.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json.Serialization;
+using Commander.Models;
 
-
-namespace Commander
+namespace Commander;
+public class Startup
 {
-    public class Startup
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
+        _configuration = configuration;
+    }
+
+    private readonly IConfiguration _configuration;
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        var con = _configuration.GetConnectionString("DB_CONNECTION");
+
+        services.AddDbContext<CommanderContext>(opt => opt.UseNpgsql(con));
+
+        services.AddControllers()
+            .AddNewtonsoftJson(s =>
+                s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver());
+
+        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+        services.AddScoped<ICommandsRepo, PsqlCommandsRepo>();
+        services.AddScoped<IPlatformsRepo, PsqlPlatformsRepo>();
+        services.AddScoped<IAliasesRepo, PsqlAliasesRepo>();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
         {
-            Configuration = configuration;
+            app.UseDeveloperExceptionPage();
         }
 
-        private IConfiguration Configuration { get; }
+        // PrepDB.PrepPopulation(app);
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            var connectionString = Configuration["DB_CONNECTION_STRING"];
-            Console.WriteLine(connectionString);
-            services.AddDbContext<CommanderContext>(opt =>
-                // opt.UseNpgsql(Configuration.GetConnectionString("TestDockerConnection"))
-                opt.UseNpgsql(connectionString)
-            );
+        app.UseExceptionHandler("/error");
 
-            services.AddControllers()
-                .AddNewtonsoftJson(s =>
-                    s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver());
+        app.UseHttpsRedirection();
 
-            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
 
-            services.AddScoped<ICommandsRepo, PsqlCommandsRepo>();
-            services.AddScoped<IPlatformsRepo, PsqlPlatformsRepo>();
-            services.AddScoped<IAliasesRepo, PsqlAliasesRepo>();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            
-            PrepDB.PrepPopulation(app);
-
-            app.UseExceptionHandler("/error");
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
-        }
+        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
     }
 }
